5bdbe099361e77651b0a3e74dc7d2142ec07215c
Кадкин Роман Алексеевич <kadkin@argos.org>
2021-02-16 16:25:36 +0300

    a little more js. add github pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,29 @@
-var backHtml = document.querySelector('html');
-
-backHtml.onclick = function() {
-	var myBackground = document.documentElement.style;
+document.querySelector('html').onclick = function() {
+	var myBackground = document.documentElement.style; 
+	//не получаю backgrounImage до первого выполнения функции
     if(myBackground.backgroundImage === 'url("images/back2.png")') {
       myBackground.backgroundImage = 'url("images/back.png")';
     } else {
       myBackground.backgroundImage = 'url("images/back2.png")';
 	}
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Hello, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
 }
